acb1f08dc409480a2601448d593bac8f5a42f989
SnirDecartAI <187955124+SnirDecartAI@users.noreply.github.com>
2025-09-30 16:10:29 +0300

    $ deno run https://fabricmc.net/cli init

---
+++ src/client/kotlin/ai/decart/mirage/MirageminecraftmodClient.kt
@@ -0,0 +1,9 @@
+package ai.decart.oasis
+
+import net.fabricmc.api.ClientModInitializer
+
+object MirageminecraftmodClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
